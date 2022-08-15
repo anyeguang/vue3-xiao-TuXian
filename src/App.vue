4b565cb25ce9d79a1c5b3367c218e14ba266e350
anyeguang <2466831156@qq.com>
2022-08-15 11:49:13 +0800

    配置less

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,6 @@ onMounted(async () => {
   <h1>Vue3+TS小兔鲜儿</h1>
 </template>
 <style lang="less" scoped>
-// 必须导入 variables.less
-@import '@/assets/styles/variables.less';
-@import '@/assets/styles/mixins.less';
 h1 {
   background-color: @warnColor;
   .hoverShadow();
