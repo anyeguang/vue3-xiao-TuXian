4a1fc6e44422f622c564d877c857d8fb4d12839b
anyeguang <2466831156@qq.com>
2022-08-15 11:41:23 +0800

    配置axios和less

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import request from './utils/request';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  const res = await request.get('/home/index');
+  console.log(res);
+});
+</script>
 
 <template>
-  <h1>Vue3+TS+VueRouter4+Pinia-新版小兔鲜儿</h1>
+  <h1>Vue3+TS小兔鲜儿</h1>
 </template>
-<style scoped></style>
+<style lang="less" scoped>
+// 必须导入 variables.less
+@import '@/assets/styles/variables.less';
+@import '@/assets/styles/mixins.less';
+h1 {
+  background-color: @warnColor;
+  .hoverShadow();
+}
+</style>
